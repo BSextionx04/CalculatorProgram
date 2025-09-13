7b56d57e7e495b27ed63bcf719ccda815cd3b566
BSextion <bsextion1004@gmail.com>
2025-09-13 14:31:15 -0500

    Renaming files to relevant titles of use

---
+++ CalculatorApp/CalculatorMain.java
@@ -0,0 +1,8 @@
+package CalculatorApp;
+
+public class CalculatorMain {
+    public static void main(String[] args) {
+        System.out.println("Will you be inputting int or double values? (Use the menu that follows.)");
+        CalculatorRunner.typeMenu();
+    }
+}
